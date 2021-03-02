13aee3ce398fbf492c2209c0a15e384bf3df4e1b
Spencer Stolworthy <sdstolworthy@gmail.com>
2021-03-02 08:46:07 -0700

    user story readme added

--- test_palindrome.py
+++ test_palindrome.py
@@ -3,11 +3,4 @@ import palindrome
 
 
 class TestPalindrome(unittest.TestCase):
-    def test_is_true_if_word_is_palindrome(self):
-        self.assertTrue(palindrome.is_palindrome("racecar"))
-
-    def test_is_false_if_word_is_not_palindrome(self):
-        self.assertFalse(palindrome.is_palindrome("penguin"))
-
-    def test_can_identify_numeric_palindromes(self):
-        self.assertTrue(palindrome.is_palindrome(9009))
+    pass
