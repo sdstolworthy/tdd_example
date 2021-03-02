85a792ef94b1811bb096e3e701d11de0929760ea
Spencer Stolworthy <sdstolworthy@gmail.com>
2021-03-02 12:25:39 -0700

    assert typeerror

--- palindrome.py
+++ palindrome.py
@@ -4,18 +4,21 @@ from typing import Union
 def _convert_to_string(subject: any) -> str:
     subject_type = type(subject)
     if subject_type == int or subject_type == float:
-        return str(subject).replace('.','')
+        return str(subject).replace('.', '')
     raise TypeError('%s is not a supported type' % type(subject))
-    
+
+
 def _verify_str_is_palindrome(subject: str) -> bool:
     if len(subject) < 2:
         return False
     return subject[::-1] == subject
 
+
 def _normalize_str(string: str) -> str:
     return string.lower().strip()
 
-def is_palindrome(subject: any) -> bool:
+
+def is_palindrome(subject: Union[str, float, int]) -> bool:
     if type(subject) != str:
         subject = _convert_to_string(subject)
     subject = _normalize_str(subject)
--- test_palindrome.py
+++ test_palindrome.py
@@ -11,12 +11,16 @@ class TestPalindrome(unittest.TestCase):
 
     def test_can_identify_numeric_palindromes(self):
         self.assertTrue(palindrome.is_palindrome(9009))
-    
+
     def test_single_character_string_is_not_a_palindrome(self):
         self.assertFalse(palindrome.is_palindrome('a'))
 
     def test_numerical_palindromes_ignore_decimal_point(self):
         self.assertTrue(palindrome.is_palindrome(900.9))
-    
+
     def test_case_is_ignored(self):
         self.assertTrue(palindrome.is_palindrome('RaceCar'))
+
+    def test_incorrect_input_type_throws_error(self):
+        with self.assertRaises(TypeError):
+            palindrome.is_palindrome(True)
